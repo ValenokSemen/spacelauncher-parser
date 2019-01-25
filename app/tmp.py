20c353691d0c71122d360f189dbe12129f3e3fa7
pxbdpacn <33340607+pxbdpacn@users.noreply.github.com>
2019-01-25 16:55:56 +0300

    split juniper code

--- app/tmp.py
+++ app/tmp.py
@@ -1,7 +1,8 @@
 import json
+import os.path
+import re
 import sys
 from contextlib import closing
-import os.path
 
 from bs4 import BeautifulSoup
 from requests import get
@@ -14,15 +15,50 @@ try:
     print('ptvsd is started')
 except:
     print('ptvsd not working')
-        
+
+def get_depth(element):
+    depth = 0
+    if ("ind-statement" in element.get('class')):
+        depth += 1
+    parent = element.parent
+    while (parent.name != 'sw-code'):
+        depth += 1      
+        parent = parent.parent
+    return depth
+
+
+def clean_data(param):
+    pattern = re.compile('(\n\s+|\n)')
+    return pattern.sub(' ', param).replace(' {', '')
+
+def is_split_data(value):
+    # ^(\w[0-9a-zA-Z-_]+)|(\[.*?\])
+    match = re.search(r'\[.*?\]', value) 
+    return(True if match else False)
+
+def set_split_data(value):
+    match = re.search(r'\[.*?\]', value) 
+    if match:
+        return re.split('\s+|\[|\]', match[0])     
+
 if __name__ == "__main__":
     rel_path = os.path.abspath(os.path.dirname(__file__))
     abs_path = os.path.join(rel_path, "test_files/tmp.html")
     with open(abs_path, 'r') as f:
         html = BeautifulSoup(f, 'lxml')
         get_syntax = html.select_one("sw-code")
-        
+        crumbs = []
+        elem_crumbs = ['']*20
         for index, value in enumerate(get_syntax.select(".statement"), 1):
-            print(value.text)
-
-    
+            depth = get_depth(value)
+            value = clean_data(value.text)
+            
+            if is_split_data(value):
+                split_array = [i for i in set_split_data(value) if i]
+            
+            if re.match(r'^}', value) is None:
+                elem_crumbs[depth] = value
+                elem_crumbs[depth+1:] = ['']*(20-depth-1)
+                crumbs.append('/'.join([e for e in elem_crumbs if e]))
+                
+#                 # print('{} is depth {}'.format(value, depth))
