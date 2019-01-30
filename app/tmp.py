ea92e516f488007aa2d859fe36b515f61427047b
pxbdpacn <33340607+pxbdpacn@users.noreply.github.com>
2019-01-30 17:31:59 +0300

    set class

--- app/tmp.py
+++ app/tmp.py
@@ -8,6 +8,8 @@ from bs4 import BeautifulSoup
 from requests import get
 from requests.exceptions import RequestException
 
+# https://github.com/pkolt/design_patterns/blob/master/generating/builder.py
+
 try:
     import ptvsd
     ptvsd.enable_attach(address=('0.0.0.0', 5678), redirect_output=True)
@@ -40,10 +42,11 @@ class Rematcher(object):
 
 class Breadcrumbs(object):
 
-    __depth = 0
+    __crumbs = []
+    __elem_crumbs = ['']*20
 
     def __init__(self, html):
-        self.statement = html
+        self.statement = html.select_one("sw-code").select(".statement")
     
     @property
     def statement(self):
@@ -65,86 +68,56 @@ class Breadcrumbs(object):
         # statement setter
         self.__depth = depth
 
-    def set_statement_depth(self):
-        if ("ind-statement" in self.statement.get('class')):
+    def set_statement_depth(self, statement_element):
+        self.depth = 0
+        if ("ind-statement" in statement_element.get('class')):
             self.depth += 1 
-        upper_parent = self.__statement.parent
+        upper_parent = statement_element.parent
         while (upper_parent.name != 'sw-code'):
             self.depth += 1      
             upper_parent = upper_parent.parent
 
-    def clean_statement(self):
+    def clean_statement(self, statement_element):
         pattern = re.compile(r'(\n\s+|\n)')
-        return pattern.sub(' ', self.statement.text).replace(' {', '')
+        return pattern.sub(' ', statement_element.text).replace(' {', '')
 
-    def get_breadcrumbs(self, parameter_list):
-        for index, value in enumerate():
-            pass
+    def get_breadcrumbs(self):
+        for statement_el in self.statement:
+            self.set_statement_depth(statement_el)
+            clean_statement_el = self.clean_statement(statement_el)
+            m = Rematcher(clean_statement_el)
+            if m.is_match(r'(\(|\||\[)'):
+                m.group(r'^(\w[0-9a-zA-Z-_]+)')
+                m.split()
+                if m.get_header:
+                    for i, value in enumerate(m.mylist, 1):
+                        if (i == 2):
+                            self.depth += 1
+                        self.set_breadcrumbs(value)
+                else:
+                    for i, value in enumerate(m.mylist, 1):
+                        self.set_breadcrumbs(value)
 
+                continue
+            if re.match(r'^}', clean_statement_el) is None:
+                self.set_breadcrumbs(clean_statement_el)
+        return self.__crumbs
 
-def get_depth(element):
-    depth = 0
-    if ("ind-statement" in element.get('class')):
-        depth += 1
-    parent = element.parent
-    while (parent.name != 'sw-code'):
-        depth += 1      
-        parent = parent.parent
-    return depth
+    
+    def set_breadcrumbs(self, value):
+        self.__elem_crumbs[self.depth] = value
+        self.__elem_crumbs[self.depth+1:] = ['']*(20-self.depth-1)
+        self.__crumbs.append('/'.join([e for e in self.__elem_crumbs if e]))
 
-
-def clean_data(param):
-    pattern = re.compile('(\n\s+|\n)')
-    return pattern.sub(' ', param).replace(' {', '')
-
-def is_split_data(value):
-    match = re.search(r'(\(|\||\[)', value) 
-    return bool(match)
-
-def set_split_data(value):
-    match = re.search(r'^(\w[0-9a-zA-Z-_]+)|(\[.*?\])|(\(.*?\))', value) 
-
-    if match.group(0):
-        pass
-
-    # if match:
-    #     return re.split('\s+|\[|\]', match[0])     
-
-# https://github.com/pkolt/design_patterns/blob/master/generating/factory_method.py
-
-if __name__ == "__main__":
+def main():
     rel_path = os.path.abspath(os.path.dirname(__file__))
     abs_path = os.path.join(rel_path, "test_files/tmp.html")
     with open(abs_path, 'r') as f:
         html = BeautifulSoup(f, 'lxml')
-        get_syntax = html.select_one("sw-code")
-        crumbs = []
-        elem_crumbs = ['']*20
-        for index, value in enumerate(get_syntax.select(".statement"), 1):
-            depth = get_depth(value)
-            value = clean_data(value.text)
-            
-            m = Rematcher(value)
-            if m.is_match(r'(\(|\||\[)'):
-               m.group(r'^(\w[0-9a-zA-Z-_]+)')
-               m.split()
-               if m.get_header:
-                   for i, v in enumerate(m.mylist, 1):
-                       if (i == 2):
-                           depth += 1
-                       elem_crumbs[depth] = v
-                       elem_crumbs[depth+1:] = ['']*(20-depth-1)
-                       crumbs.append('/'.join([e for e in elem_crumbs if e])) 
-               else:
-                   for i, arg in enumerate(m.mylist, 1):
-                       elem_crumbs[depth] = arg
-                       elem_crumbs[depth+1:] = ['']*(20-depth-1)
-                       crumbs.append('/'.join([e for e in elem_crumbs if e]))
-               continue
-            
-            if re.match(r'^}', value) is None:
-                elem_crumbs[depth] = value
-                elem_crumbs[depth+1:] = ['']*(20-depth-1)
-                crumbs.append('/'.join([e for e in elem_crumbs if e]))
-                
-#                 # print('{} is depth {}'.format(value, depth))
+        breadcrumbs = Breadcrumbs(html)
+        breadcrumbs_list = breadcrumbs.get_breadcrumbs()
+        print(breadcrumbs_list)
+
+if __name__ == "__main__":
+    main()
+    # print('{} is depth {}'.format(value, depth))
