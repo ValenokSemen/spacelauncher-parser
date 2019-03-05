36a63520c32ce9454772f6515db9ecaf34e687d4
ValenokSemen <homu@cartelera.org>
2019-03-06 01:13:24 +0300

    Edit advanced filter in tmp.py

--- app/Untitled-1.py
+++ app/Untitled-1.py
@@ -1,34 +1,115 @@
 import re
 
+class Separator(object):
+    def get_pattern(self):
+        raise NotImplementedError
+
+class VlineSeparator(Separator):
+    def get_pattern(self):
+        vlineSeparator = re.compile(r'\|')
+        return vlineSeparator
+
+
+class SpaceSeparator(Separator):
+    def get_pattern(self):
+        spaceSeparator = re.compile(r'\s+')
+        return spaceSeparator
+
 class Handler(object):
     """Абстрактный класс обработчика"""
+
+    def __init__(self):
+        pass
+    
+    def atribute_list(self, content):
+        tmp = []
+        matches = self.get_pattern().finditer(content)
+        for i, v in enumerate(matches):
+            if not tmp:
+                for num, match in enumerate(matches, start=0):
+                    if num == i:
+                        for s in self.separate_atribute(match, content):
+                            tmp.append(s)
+            else:
+                hh = []
+                for t in tmp:
+                    for matchNum, match in enumerate(self.get_pattern().finditer(t), start=0):
+                        if matchNum == i:
+                            for s in self.separate_atribute(match, t):
+                                hh.append(s)
+                    
+                tmp = hh
+        return tmp
+    
+    def separate_atribute(self,  m, init_string):
+        if self.get_separates():
+             for separator in self.get_separates():
+                separator_pattern = separator.get_pattern()
+                if separator_pattern.search(m.group()):
+                    splitlist = [s for s in separator_pattern.split(m.group()) if s]
+                    for s in splitlist:
+                        s_wo_space = s.strip()
+                        if s_wo_space is not '':
+                            yield (init_string[:m.start()] + s_wo_space + init_string[m.end():])
+
     def handle(self, content):
-        raise NotImplementedError()
- 
+        raise NotImplementedError   
+
+    def get_pattern(self):
+        raise NotImplementedError
+    
+    def get_separates(self):
+        raise NotImplementedError
  
 class CircleBracket(Handler):
     """Обработчик для ()"""
-    __cercalBracket = re.compile(r'(?<=\()(?:[^)(]|\((?:[^)(]|\([^)(]*\))*\))*(?=\))')
+    __pattern = re.compile(r'(?<=\()(?:[^)(]|\((?:[^)(]|\([^)(]*\))*\))*(?=\))')
+
+    def __init__(self, separates = None):
+        super().__init__()
+        self.__separates = []
+        if separates is not None:
+            self.__separates += separates
+    
+    def get_separates(self):
+        return self.__separates
+    
+    def get_pattern(self):
+        return self.__pattern
 
     def handle(self, content):
-        if content == 404:
-            return 'Страница не найдена'
- 
+        attributes = []
+        if isinstance(content, str):
+            if not attributes:
+                atribute_list = self.atribute_list(content)
+                if atribute_list:
+                    attributes = [attr for attr in atribute_list]
+                else:
+                    attributes.append(content)
+        elif isinstance(content, list):
+            new_arr = []
+            for target in content:
+              for attr in self.atribute_list(target):
+                  if attr:
+                      new_arr.append(attr)
+            if new_arr:
+                attributes = new_arr
+        return attributes if attributes else content
+
 class Comment(Handler):
     """Обработчик для удаления комментария"""
-    __comment = re.compile(r'\(([A-Z].[^)(]*?)\)')
-    
-    def handle(self, content):
-        if content == 404:
-            return 'Страница не найдена'
-  
-class TriangleBracket(Handler):
-    """Обработчик для <>"""
-    __triangleBracket = re.compile(r'(?<=\<)(.*?)(?=\>)')
+    pattern = re.compile(r'\(([A-Z].[^)(]*?)\)')
 
     def handle(self, content):
-        if content == 500:
-            return 'Ошибка сервера'
+        content = self.pattern.sub('', content)
+        return content
+  
+    def get_pattern(self):
+        return self.pattern
+class TriangleBracket(Handler):
+    """Обработчик для <>"""
+    __pattern = re.compile(r'(?<=\<)(.*?)(?=\>)')
+
 
 class ContentFilter(object):
     def __init__(self, filters=None):
@@ -37,14 +118,16 @@ class ContentFilter(object):
             self._filters += filters
  
     def filter(self, content):
+        initial_string = content
         for filter in self._filters:
-            content = filter.handle(content)
+            if filter.get_pattern().search(initial_string):
+                content = filter.handle(content)
         return content
  
 filter = ContentFilter([
-                Comment()
-                CircleBracket(),
-                TriangleBracket()
+                Comment(),
+                CircleBracket([VlineSeparator(), SpaceSeparator()])
                 ])
 content = "clear bfd session <logical-system (all | logical-system-name)>"
 filtered_content = filter.filter(content)
+print(filtered_content)
--- app/tmp.py
+++ app/tmp.py
@@ -229,6 +229,131 @@ def test():
             #             print('\n'.join(map(str, h.hierarhy_pathlist)))
                                                                      
 
+class Separator(object):
+    def get_pattern(self):
+        raise NotImplementedError
+
+class VlineSeparator(Separator):
+    def get_pattern(self):
+        vlineSeparator = re.compile(r'\|')
+        return vlineSeparator
+
+
+class SpaceSeparator(Separator):
+    def get_pattern(self):
+        spaceSeparator = re.compile(r'\s+')
+        return spaceSeparator
+
+class Handler(object):
+    """Абстрактный класс обработчика"""
+
+    def __init__(self):
+        pass
+    
+    def atribute_list(self, content):
+        tmp = []
+        matches = self.get_pattern().finditer(content)
+        for i, v in enumerate(matches):
+            if not tmp:
+                for num, match in enumerate(self.get_pattern().finditer(content), start=0):
+                    if num == i:
+                        for s in self.separate_atribute(match, content):
+                            tmp.append(s)
+            else:
+                hh = []
+                for t in tmp:
+                    for matchNum, match in enumerate(self.get_pattern().finditer(t), start=0):
+                        if matchNum == i:
+                            for s in self.separate_atribute(match, t):
+                                hh.append(s)
+                    
+                tmp = hh
+        return tmp
+    
+    def separate_atribute(self,  m, init_string):
+        if self.get_separates():
+             for separator in self.get_separates():
+                separator_pattern = separator.get_pattern()
+                if separator_pattern.search(m.group()):
+                    splitlist = [s for s in separator_pattern.split(m.group()) if s]
+                    for s in splitlist:
+                        s_wo_space = s.strip()
+                        if s_wo_space is not '':
+                            yield (init_string[:m.start()] + s_wo_space + init_string[m.end():])
+                    break
+                
+
+    def handle(self, content):
+        raise NotImplementedError   
+
+    def get_pattern(self):
+        raise NotImplementedError
+    
+    def get_separates(self):
+        raise NotImplementedError
+ 
+class CircleBracket(Handler):
+    """Обработчик для ()"""
+    __pattern = re.compile(r'(?<=\()(?:[^)(]|\((?:[^)(]|\([^)(]*\))*\))*(?=\))')
+
+    def __init__(self, separates = None):
+        super().__init__()
+        self.__separates = []
+        if separates is not None:
+            self.__separates += separates
+    
+    def get_separates(self):
+        return self.__separates
+    
+    def get_pattern(self):
+        return self.__pattern
+
+    def handle(self, content):
+        attributes = []
+        if isinstance(content, str):
+            if not attributes:
+                atribute_list = self.atribute_list(content)
+                if atribute_list:
+                    attributes = [attr for attr in atribute_list]
+                else:
+                    attributes.append(content)
+        elif isinstance(content, list):
+            new_arr = []
+            for target in content:
+              for attr in self.atribute_list(target):
+                  if attr:
+                      new_arr.append(attr)
+            if new_arr:
+                attributes = new_arr
+        return attributes if attributes else content
+
+class Comment(Handler):
+    """Обработчик для удаления комментария"""
+    pattern = re.compile(r'\(([A-Z].[^)(]*?)\)')
+
+    def handle(self, content):
+        content = self.pattern.sub('', content)
+        return content
+  
+    def get_pattern(self):
+        return self.pattern
+class TriangleBracket(Handler):
+    """Обработчик для <>"""
+    __pattern = re.compile(r'(?<=\<)(.*?)(?=\>)')
+
+
+class ContentFilter(object):
+    def __init__(self, filters=None):
+        self._filters = []
+        if filters is not None:
+            self._filters += filters
+ 
+    def filter(self, content):
+        initial_string = content
+        for filter in self._filters:
+            if filter.get_pattern().search(initial_string):
+                content = filter.handle(content)
+        return content
 
 def run():
     my_array = [
@@ -257,116 +382,18 @@ def run():
                 # "n-plus-n (Power Management);",
                 # "ethernet (Aggregated Devices) {"
     ]
-    
-    # cercalBracket = re.compile(r'(?<=\()(.*?)(?=\))')
-    cercalBracket = re.compile(r'(?<=\()(?:[^)(]+|\((?:[^)(]+|\([^)(]*\))*\))*(?=\))')
-    squareBracket = re.compile(r'(?<=\[)(.*?)(?=\])')
-    triangleBracket = re.compile(r'(?<=\<)(.*?)(?=\>)')
-    quote = re.compile(r'"(.*?)"')
 
-    comment = re.compile(r'\(([A-Z].[^)(]*?)\)')
-    
-    for target_list in my_array:
-        tmplist = []
-        if comment.search(target_list):
-            target_list = deleteComment(comment, target_list)
-        if cercalBracket.search(target_list):
-            func_res = my_funcname(cercalBracket, target_list, tmplist)
-            if isinstance(func_res, str):
-                tmplist.append(func_res)
-            elif isinstance(func_res, list):
-                tmplist = func_res
-        if squareBracket.search(target_list):
-            func_res = my_funcname(squareBracket, target_list, tmplist)
-            if isinstance(func_res, str):
-                tmplist.append(func_res)
-            elif isinstance(func_res, list):
-                tmplist = func_res
-        if triangleBracket.search(target_list):
-            func_res = my_funcname(triangleBracket, target_list, tmplist)
-            if isinstance(func_res, str):
-                tmplist.append(func_res)
-            elif isinstance(func_res, list):
-                tmplist = func_res
-        if quote.search(target_list):
-            pass
+    filter = ContentFilter([
+                Comment(),
+                CircleBracket([VlineSeparator(), SpaceSeparator()])
+                ])
+    content = "clear bfd (My Tech comment) session <logical-system (all | logical-system-name)>"
+    filtered_content = filter.filter(content)
+    print(filtered_content)
 
-        if tmplist:
-            for t in tmplist:
-                print(t)
-        else:
-            print(target_list)
-
-
-def deleteComment(patern, string):
-    return patern.sub('', string)
-
-def my_funcname(patern, string, arr):
-    if not arr:
-        arr = [attr for attr in get_atribute_list(patern, string)]
-        return arr if arr else string
-    else:
-        new_arr = []
-        for target in arr:
-            for attr in get_atribute_list(patern, target):
-                if attr:
-                    new_arr.append(attr)
-        return new_arr if new_arr else arr
-
-def get_atribute_list(patern, string):
-    matches = patern.finditer(string)
-    length = len(list(matches))  
-    tmp = []
-    i = 0
-    while i < length:
-        if not tmp:
-            for matchNum, match in enumerate(patern.finditer(string), start=0):
-                if matchNum == i:
-                    for s in funcname(match, string):
-                        tmp.append(s)
-        else:
-            hh = []
-            for t in tmp:
-                for matchNum, match in enumerate(patern.finditer(t), start=0):
-                    if matchNum == i:
-                        for s in funcname(match, t):
-                            hh.append(s)
-                   
-            tmp = hh
-        i += 1
-    return tmp
-
-def funcname(m, init_string):
-    quoteSeparator = re.compile(r'"(.*?)"')
-    spaceSeparator = re.compile(r'\s+')
-    vlineSeparator = re.compile(r'\|')
-    if quoteSeparator.search(m.group()):
-        string_generator = createStringGenerator(init_string, quoteSeparator, m)
-        for line in string_generator:
-            yield line
-    else:
-        if vlineSeparator.search(m.group()):
-            string_generator = createStringGenerator(init_string, vlineSeparator, m)
-            for line in string_generator:
-                yield line
-        elif spaceSeparator.search(m.group()):
-            string_generator = createStringGenerator(init_string, spaceSeparator, m)
-            for line in string_generator:
-                yield line
-        else:
-            # if between bracket one value
-            pass
-
-def createStringGenerator(string, separator, match):
-    splitlist = [s for s in separator.split(match.group()) if s]
-    for s in splitlist:
-        s_wo_space = s.strip()
-        if s_wo_space is not '':
-             yield (string[:match.start()] + s_wo_space + string[match.end():])
-       
 
 if __name__ == "__main__":
     # main()
-    test()
-    # run()
+    # test()
+    run()
             
